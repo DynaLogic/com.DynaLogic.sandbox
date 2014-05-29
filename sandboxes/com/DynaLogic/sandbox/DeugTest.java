c91a13adf151a7683e1a1c5300779bb66e85f1d5
johnsmith0508 <loganwaldman@gmail.com>
2014-05-29 11:46:42 -0400

    edit debug test.java
    remove blankness
    add loop with in/out (broken[?])

--- sandboxes/com/DynaLogic/sandbox/DeugTest.java
+++ sandboxes/com/DynaLogic/sandbox/DeugTest.java
@@ -1,12 +1,25 @@
 package com.DynaLogic.sandbox;
 
+import java.io.*;
+
 public class DeugTest {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws InterruptedException, IOException {
 		while (true)
 		{
-		
+			System.out.print("Input: ");
+			BufferedReader dataIn = new BufferedReader(new InputStreamReader(System.in));
+			String input = dataIn.readLine();
+			if (input =="ping")
+			{
+				System.out.println("Pong");
+			}
+			else
+			{
+				System.out.println("NO");
+				System.out.println(input);
+			}
+			Thread.sleep(1000);
 		}
 
 	}
